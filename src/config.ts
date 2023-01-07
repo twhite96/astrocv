6d0475d7abc2cb4019ecda3fce48c1f638c34621
Tiffany <hey@tiffanyrwhite.com>
2023-01-07 04:48:02 -0500

    Update

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "CV";
-export const SITE_URL = "";
+export const SITE_URL = "https://resume.tiffanyrwhite.com";
