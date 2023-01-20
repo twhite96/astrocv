36cf922356484757031029510afcf14e392dd423
Tiffany <hey@tiffanyrwhite.com>
2023-01-19 23:16:21 -0500

    Fix responsiveness

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "CV";
+export const SITE_TITLE = "Tiffany White, CV";
 export const SITE_URL = "https://resume.tiffanyrwhite.com";
