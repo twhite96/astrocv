bbe388cd9e1b1f906cfb366d82b771433c489f56
Tiffany <tiffany@tiffanywhite.dev>
2023-08-01 22:03:04 -0400

    Update something

---
+++ config/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    date: "09/2021 - 08/2022",
+    role: "Software Engineer",
+    company: "Calaxy",
+    description:
+      "Created the Session History Vue component for Calaxy's internal admin tool, as well as a Pagination Cursor Vue component to paginate users from a Rust API built by an external contractor with Calaxy. I Took Figma designs to create a social token leaderboard for Calaxy token holders with a junior developer on our team and coordinated with the junior developer to work out the layout bugs in the social token leaderboard. Refactored all of the HTML and CSS in the social token leaderboard and collaborated with a junior developer on improving the speed of our API calls for the social token leaderboard.",
+  },
+  {
+    id: 2,
+    date: "03/2020",
+    role: "Freelance Frontend Engineer",
+    company: "Bumped",
+    description:
+      "Refactoring bumped.com’s Gatsby site using styled-components, JavaScript, and TypeScript, with GraphQL querying our database.",
+  },
+  {
+    id: 3,
+    date: "07/2019 - 02/2020",
+    role: "Frontend Developer",
+    company: "ICF",
+    description:
+      "Responsible for rebuilding the entire UI for one government website using React, Reactstrap, and Typescript. I also built the login page and citation pages with the United States Government web design language and custom HTML for the footer and header. I was responsible for maintaining government websites with HTML 5, SASS,JavaScript and jQuery to add interactivity. I also attended weekly check-ins and daily standups to discuss the progression of our work for clients using Jira and Confluence. Got acquainted with TDD using Jenkins as continuous integration and testing before deploying to production.",
+  },
+];
+
+export default data
